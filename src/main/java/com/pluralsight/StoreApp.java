781b8a76ba45193d0116692797a4000eb7a66e4f
margaretpalma <mpalma1496@gmail.com>
2025-10-08 11:22:23 -0400

    changes in inventory.add to match workbook

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -5,30 +5,34 @@ import java.util.Scanner;
 public class StoreApp {
     public static void main(String[] args) {
 
+        //arraylist
         ArrayList<Product> inventory = getInventory();
 
         System.out.println("Items in Inventory: ");
 
-        //todo: loops
+        //todo: loops and display
+
+
+        }
+
+
 
     }
 
+
+    //products
     public static ArrayList<Product> getInventory(){
         ArrayList<Product> inventory = new ArrayList<Product>();
+//match the workbook ",add"
 
-        Product p1 = new Product(8500, "vinyl", 25.89f);
-                inventory.add(p1);
-
-        Product p2 = new Product(8501, "controller", 75.99);
-                inventory.add(p2);
-
-        Product p3 = new Product(8502,"watch", 85.99);
-                inventory.add(p3);
-
-        Product p4 = new Product(8503,"keyboard", 99.99);
+        inventory.add(new Product(8500, "vinyl", 25.89f));
+        inventory.add(new Product(8501, "controller", 75.99f));
+        inventory.add(new Product(8502, "watch", 85.99f));
+        inventory.add(new Product(8503, "keyboard", 99.99f));
+        inventory.add(new Product(8504, "airpods", 105.99f));
+//return
+        return inventory;
     }
-
-
 
 
 }
